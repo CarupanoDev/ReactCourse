40c7c0f83eddc02d7f8308ae5e77afd8a599721a
Santiago Medina <carupanodev@gmail.com>
2022-02-09 09:09:42 -0400

    Fix: fix typo on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import HelloWorld from './documentation/HelloWorld';
 import Toogle from './documentation/handling-events/Toggle';
 import Calculator from './documentation/lifting-up-state/Calculator';
 import Clock from './documentation/state-and-lifecycle/Clock';
@@ -8,7 +7,7 @@ import Clock from './documentation/state-and-lifecycle/Clock';
 const App = () => {
   return (
     <div className='App'>
-      <HelloWorld />
+      <Clock />
     </div>
   );
 }
